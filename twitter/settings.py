bf9f7d473bafa0d179aad394f9960b406c8e988d
ni <ocala352615@gmail.com>
2023-07-18 19:11:42 -0400

    04

--- twitter/settings.py
+++ twitter/settings.py
@@ -36,7 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    # rest frameworks
+    'rest_framework',
+    # project apps
+    'accounts'
 ]
 
 REST_FRAMEWORK = {
